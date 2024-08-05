6bcb287586060fac9c493a7792f3fc224ca320a8
Ernest Kumashie <mauly.dev@gmail.com>
2024-08-05 23:20:40 +0000

    static path

--- core/settings.py
+++ core/settings.py
@@ -136,10 +136,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-MEDIA_ROOT = "media/"
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
