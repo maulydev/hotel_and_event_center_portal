7018bbe276b17442210d2f48007b1360dcf99cda
Ernest Kumashie <mauly.dev@gmail.com>
2024-08-07 16:16:09 +0000

    allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0i=2ehvuire5q9irqnyucqg0z)l3$#ls&dby@tr)*66-((d83y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1"]
 
 
 # Application definition
