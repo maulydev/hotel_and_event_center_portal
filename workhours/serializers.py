adda8f26721ad6d2ee735720572aa557991b194c
Ernest Kumashie <mauly.dev@gmail.com>
2024-08-08 12:00:33 +0000

    workhours refined

--- workhours/serializers.py
+++ workhours/serializers.py
@@ -1,8 +1,25 @@
 from rest_framework import serializers
 from .models import Workhours
 
-
 class WorkhoursSerializers(serializers.ModelSerializer):
+    workhours = serializers.SerializerMethodField()
+
     class Meta:
         model = Workhours
-        fields = '__all__'
+        exclude = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+    def get_workhours(self, obj):
+        return [
+            {"day": "Sunday", "period": obj.sunday},
+            {"day": "Monday", "period": obj.monday},
+            {"day": "Tuesday", "period": obj.tuesday},
+            {"day": "Wednesday", "period": obj.wednesday},
+            {"day": "Thursday", "period": obj.thursday},
+            {"day": "Friday", "period": obj.friday},
+            {"day": "Saturday", "period": obj.saturday},
+        ]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['workhours'] = self.get_workhours(instance)
+        return representation
