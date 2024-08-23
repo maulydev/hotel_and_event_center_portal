d31b3d54a5545091201813723fe7d24ea13312db
Ernest Kumashie <mauly.dev@gmail.com>
2024-08-23 07:17:34 +0000

    .env set

--- lib/otp.py
+++ lib/otp.py
@@ -1,9 +1,17 @@
+import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+SENDER_ID = os.getenv('SENDER_ID')
+
 
 def send_otp_sms(phone_number, otp):
     # url = 'https://api.msg91.com/api/sendhttp.php'
-    api_key = ''
-    sender_id = 'ercodr(TM)'
+    api_key = API_KEY
+    sender_id = SENDER_ID
     message = f'Your Verfication code is: {otp}'
     recipient = phone_number
     url = f'https://sms.arkesel.com/sms/api?action=send-sms&api_key={api_key}&to={recipient}&from={sender_id}&sms={message}'
